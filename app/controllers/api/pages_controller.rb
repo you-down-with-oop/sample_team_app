3677126ffc1c4b5b0bc76e625e0af4937107a07a
Siva <carah.tyche@gmail.com>
2019-08-18 09:45:24 -0500

    Like a gogurt but to stay

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,5 @@
 class Api::PagesController < ApplicationController
   def index
-    render json: { message: "Peter Jang says hello!" }
+    render json: { message: "So it's... Continental?" }
   end
 end
